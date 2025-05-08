71db3e7ef350a05abf513377045067b0b9087dc1
Habrhammer <iamsorry1885@gmail.com>
2025-05-08 14:01:03 +0200

    feat: add basic http server

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import http from 'http';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ message: 'Server is up and running' }));
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default server;
